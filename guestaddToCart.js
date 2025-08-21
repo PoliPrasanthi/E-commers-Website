22de5b4c90d4be5729a90a148013b39f3b672448
Prasanthi Poli <prasanthipoli2003@gmail.com>
2025-08-21 16:39:18 +0530

    Update guestaddToCart.js

--- guestaddToCart.js
+++ guestaddToCart.js
@@ -1,11 +1,4 @@
 function displayData() {
-  let CartCon = document.getElementById("items");
-  let totalPrice = document.getElementById("totalPrice");
-  let products = JSON.parse(localStorage.getItem("cart")) || [];
-  CartCon.innerHTML = "";
-  totalPrice.innerHTML = ""; 
-
-  
     CartCon.innerHTML = `<p>Cart is empty</p>`;
 
 }
